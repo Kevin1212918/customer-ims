e3f0f2301f26cab2c3c528af10e34ee487221320
Kevin Wu <kevinwu2@andrew.cmu.edu>
2025-09-01 21:51:17 -0400

    Implement loading screen

--- client/src/getCustomer.js
+++ client/src/getCustomer.js
@@ -1,56 +1,79 @@
 import { useState } from 'react';
 import { SERVER_API_URL } from './util';
 export default function GetCustomer() {
-    const [customer, setCustomer] = useState(null);
-    if (customer === null) {
-        return (
-            <div>
-                <SearchCustomer setCustomer={setCustomer} />
-                Customer not found!
-            </div>
-        )
-    }
+  const [isLoading, setIsLoading] = useState(false);
+  const [customer, setCustomer] = useState(null);
+  if (isLoading) {
     return (
-        <div>
-            <SearchCustomer setCustomer={setCustomer} />
-            <ul>
-                <li>ID: {customer.id}</li>
-                <li>Name: {customer.name}</li> 
-                <li>Email: {customer.email}</li>
-                <li>Address: {customer.address}</li>
-                <li>Last updated: {new Date(customer.updatedAt).toLocaleString("en-US")}</li>
-                <li>Created on: {new Date(customer.createdAt).toLocaleString("en-US")}</li>
-            </ul>
-        </div>
-    );
+      <div>
+        <SearchCustomer setCustomer={setCustomer} setIsLoading={setIsLoading} />
+        Loading...
+      </div>
+    )
+  }
+  return (
+    <div>
+      <SearchCustomer setCustomer={setCustomer} setIsLoading={setIsLoading} />
+      <CustomerDetail customer={customer} />
+    </div>
+  );
 }
 
-function SearchCustomer(props) {
-    function search(id_string) {
-        if (id_string === "") {
-            return;
-        }
-        const id = Number(id_string);
-        if (isNaN(id)) {
-            return;
-        }
-        fetch(`${SERVER_API_URL}/customer/${id}`, {method: "GET"})
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    props.setCustomer(result)
-                },
-                (error) => { 
-                    props.setCustomer(null)
-                    console.log(`fetch failed: ${error}`);
-                }
-            );
-    }
-
+function CustomerDetail(props) {
+  const customer = props.customer;
+  if (customer === null) {
     return (
-        <div>
-            <label>Customer ID: </label>
-            <input onChange={e => search(e.target.value)} />
-        </div>
+      <div>
+        Customer not found!
+      </div>
     )
+  }
+
+  return (
+    <>
+      <ul>
+        <li>ID: {customer.id}</li>
+        <li>Name: {customer.name}</li>
+        <li>Email: {customer.email}</li>
+        <li>Address: {customer.address}</li>
+        <li>Last updated: {new Date(customer.updatedAt).toLocaleString("en-US")}</li>
+        <li>Created on: {new Date(customer.createdAt).toLocaleString("en-US")}</li>
+      </ul>
+    </>
+  );
+
+
+}
+function SearchCustomer(props) {
+  function search(id_string) {
+    if (id_string === "") {
+      return;
+    }
+    const id = Number(id_string);
+    if (isNaN(id)) {
+      return;
+    }
+    props.setIsLoading(true);
+    fetch(`${SERVER_API_URL}/customer/${id}`, { method: "GET" })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          props.setCustomer(result);
+        },
+        (error) => {
+          props.setCustomer(null)
+          console.log(`fetch failed: ${error}`);
+        }
+      )
+      .then(
+        () => { props.setIsLoading(false); }
+      );
+  }
+
+  return (
+    <div>
+      <label>Customer ID: </label>
+      <input onChange={e => search(e.target.value)} />
+    </div>
+  )
 }
--- client/src/getCustomers.js
+++ client/src/getCustomers.js
@@ -1,54 +1,62 @@
 import { useEffect, useState } from 'react';
 import { SERVER_API_URL } from './util';
 export default function GetCustomers() {
-    const [customers, setCustomers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [customers, setCustomers] = useState([]);
 
-    useEffect(() => {
-        fetch(`${SERVER_API_URL}/customers`, {method: "GET"})
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    setCustomers(result)
-                },
-                (error) => { 
-                    console.log(`fetch failed: ${error}`);
-                }
-            );
-    }, [])
+  useEffect(() => {
+    fetch(`${SERVER_API_URL}/customers`, { method: "GET" })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setCustomers(result);
+        },
+        (error) => {
+          console.log(`fetch failed: ${error}`);
+        }
+      ).then(
+        () => { setIsLoading(false); }
+      );
+  }, [])
 
-    if(customers.length === 0) {
-        return (
-            <div>Create a customer first!</div>
-        )
-    }
-
-    const customerOverviews = customers.map((customer) => {
-        return <CustomerOverview customer={customer} />
-    })
-
+  if (isLoading) {
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Last Updated</th>
-                </tr>
-            </thead>
-            <tbody>
-                {customerOverviews}
-            </tbody>
-        </table>
-    );
+      <div>Loading ...</div>
+    )
+  }
+  if (customers.length === 0) {
+    return (
+      <div>Create a customer first!</div>
+    )
+  }
+
+  const customerOverviews = customers.map((customer) => {
+    return <CustomerOverview customer={customer} />
+  })
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Last Updated</th>
+        </tr>
+      </thead>
+      <tbody>
+        {customerOverviews}
+      </tbody>
+    </table>
+  );
 }
 
 function CustomerOverview(props) {
-    const customer = props.customer;
-    return (
-        <tr>
-            <td>{customer.id}</td>
-            <td>{customer.name}</td>
-            <td>{new Date(customer.updatedAt).toLocaleString("en-US")}</td>
-        </tr>
-    )
+  const customer = props.customer;
+  return (
+    <tr>
+      <td>{customer.id}</td>
+      <td>{customer.name}</td>
+      <td>{new Date(customer.updatedAt).toLocaleString("en-US")}</td>
+    </tr>
+  )
 }
